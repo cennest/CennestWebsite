8f1259dbe14c05207e802f494af6bbd64b54f825
chandrahas.khapekar <chandrahas.khapekar@harriersys.com>
2014-02-19 10:42:01 +0000

    Task :- In ContactUs Page show recent three blogs heading which will be linkable

--- CennestWebsite/CennestWebsite/Contact.aspx.cs
+++ CennestWebsite/CennestWebsite/Contact.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RssToolkit.Rss;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,35 @@ namespace CennestWebsite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                RssDocument rss = RssDocument.Load(new Uri("http://cennest.com/?feed=rss2"));
+                if (rss != null && rss.Channel != null && rss.Channel.Items != null)
+                {
+                    ViewState["BlogDate1"] = String.Format("Posted on {0:M} {0:dd}, {0:yyyy}", rss.Channel.Items[0].PubDateParsed.ToLocalTime());
+                    ViewState["BlogTitle1"] = rss.Channel.Items[0].Title;
+                    ViewState["Link1"] = rss.Channel.Items[0].Link;
 
+                    if (rss.Channel.Items.Count > 1)
+                    {
+                        ViewState["BlogDate2"] = String.Format("Posted on {0:M} {0:dd}, {0:yyyy}", rss.Channel.Items[1].PubDateParsed.ToLocalTime());
+                        ViewState["BlogTitle2"] = rss.Channel.Items[1].Title;
+                        ViewState["Link2"] = rss.Channel.Items[1].Link;
+                    }
+
+                    if (rss.Channel.Items.Count > 2)
+                    {
+                        ViewState["BlogDate3"] = String.Format("Posted on {0:M} {0:dd}, {0:yyyy}", rss.Channel.Items[2].PubDateParsed.ToLocalTime());
+                        ViewState["BlogTitle3"] = rss.Channel.Items[2].Title;
+                        ViewState["Link3"] = rss.Channel.Items[2].Link;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
