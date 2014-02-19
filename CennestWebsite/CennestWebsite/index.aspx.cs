624fe24b9f7ff1566e52646921cd17c0ffaa7fe2
chandrahas.khapekar <chandrahas.khapekar@harriersys.com>
2014-02-19 09:18:24 +0000

    Cennest Website :- In Home Page(index.html) replace Universal Design and Easy to Edit with recent two blogs

--- CennestWebsite/CennestWebsite/index.aspx.cs
+++ CennestWebsite/CennestWebsite/index.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using RssToolkit.Rss;
 
 namespace CennestWebsite
 {
@@ -11,7 +12,29 @@ namespace CennestWebsite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                RssDocument rss = RssDocument.Load(new Uri("http://cennest.com/?feed=rss2"));
+                if (rss != null && rss.Channel != null && rss.Channel.Items != null)
+                {
+                    ViewState["BlogDate1"] = String.Format("Posted on {0:M} {0:dd}, {0:yyyy}", rss.Channel.Items[0].PubDateParsed.ToLocalTime());
+                    ViewState["BlogTitle1"] = rss.Channel.Items[0].Title;
+                    ViewState["BlogDescription1"] = rss.Channel.Items[0].Description.Trim().Replace("[&#8230;]", "");
+                    ViewState["Link1"] = rss.Channel.Items[0].Link;
 
+                    if (rss.Channel.Items.Count > 1)
+                    {
+                        ViewState["BlogDate2"] = String.Format("Posted on {0:M} {0:dd}, {0:yyyy}", rss.Channel.Items[1].PubDateParsed.ToLocalTime());
+                        ViewState["BlogTitle2"] = rss.Channel.Items[1].Title;
+                        ViewState["BlogDescription2"] = rss.Channel.Items[1].Description.Trim().Replace("[&#8230;]", "");
+                        ViewState["Link2"] = rss.Channel.Items[1].Link;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
