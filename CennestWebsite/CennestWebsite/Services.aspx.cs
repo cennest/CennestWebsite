2c43ed3063cc99edd0729330b7719274efee6499
anshulee@cennest.com <anshulee@cennest.com@18fda3b2-8011-4f29-a91c-af61549d47c6>
2014-05-14 13:26:41 +0000

    Added Service.aspx page.

---
+++ CennestWebsite/CennestWebsite/Services.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RssToolkit.Rss;
+using TweetSharp;
+
+namespace CennestWebsite
+{
+    public partial class index : System.Web.UI.Page
+    {
+        
+    }
+}
