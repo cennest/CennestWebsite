0677ae9db69e25a128b2c2545fb872bab3de0a50
anshulee@cennest.com <anshulee@cennest.com@18fda3b2-8011-4f29-a91c-af61549d47c6>
2014-05-29 13:40:22 +0000

    Uncommented the code.

--- CennestWebsite/CennestWebsite/Contact.aspx.cs
+++ CennestWebsite/CennestWebsite/Contact.aspx.cs
@@ -14,28 +14,28 @@ namespace CennestWebsite
         {
             try
             {
-                RssDocument rss = RssDocument.Load(new Uri("http://cennest.com/?feed=rss2"));
-                if (rss != null && rss.Channel != null && rss.Channel.Items != null)
-                {
-                    ViewState["BlogDate1"] = String.Format("Posted on {0:M} {0:dd}, {0:yyyy}", rss.Channel.Items[0].PubDateParsed.ToLocalTime());
-                    ViewState["BlogTitle1"] = rss.Channel.Items[0].Title;
-                    ViewState["Link1"] = rss.Channel.Items[0].Link;
+                //RssDocument rss = RssDocument.Load(new Uri("http://cennest.com/?feed=rss2"));
+                //if (rss != null && rss.Channel != null && rss.Channel.Items != null)
+                //{
+                //    ViewState["BlogDate1"] = String.Format("Posted on {0:M} {0:dd}, {0:yyyy}", rss.Channel.Items[0].PubDateParsed.ToLocalTime());
+                //    ViewState["BlogTitle1"] = rss.Channel.Items[0].Title;
+                //    ViewState["Link1"] = rss.Channel.Items[0].Link;
 
-                    if (rss.Channel.Items.Count > 1)
-                    {
-                        ViewState["BlogDate2"] = String.Format("Posted on {0:M} {0:dd}, {0:yyyy}", rss.Channel.Items[1].PubDateParsed.ToLocalTime());
-                        ViewState["BlogTitle2"] = rss.Channel.Items[1].Title;
-                        ViewState["Link2"] = rss.Channel.Items[1].Link;
-                    }
+                //    if (rss.Channel.Items.Count > 1)
+                //    {
+                //        ViewState["BlogDate2"] = String.Format("Posted on {0:M} {0:dd}, {0:yyyy}", rss.Channel.Items[1].PubDateParsed.ToLocalTime());
+                //        ViewState["BlogTitle2"] = rss.Channel.Items[1].Title;
+                //        ViewState["Link2"] = rss.Channel.Items[1].Link;
+                //    }
 
-                    if (rss.Channel.Items.Count > 2)
-                    {
-                        ViewState["BlogDate3"] = String.Format("Posted on {0:M} {0:dd}, {0:yyyy}", rss.Channel.Items[2].PubDateParsed.ToLocalTime());
-                        ViewState["BlogTitle3"] = rss.Channel.Items[2].Title;
-                        ViewState["Link3"] = rss.Channel.Items[2].Link;
-                    }
+                //    if (rss.Channel.Items.Count > 2)
+                //    {
+                //        ViewState["BlogDate3"] = String.Format("Posted on {0:M} {0:dd}, {0:yyyy}", rss.Channel.Items[2].PubDateParsed.ToLocalTime());
+                //        ViewState["BlogTitle3"] = rss.Channel.Items[2].Title;
+                //        ViewState["Link3"] = rss.Channel.Items[2].Link;
+                //    }
 
-                }
+                //}
             }
             catch (Exception ex)
             {
