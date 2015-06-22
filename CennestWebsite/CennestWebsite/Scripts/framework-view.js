ead30e812a776c9b0dcb5e42cbf3939d333e4c7d
poulomee <poulomee@cennest.com>
2015-06-22 13:28:00 +0000

    added cennest framework page

---
+++ CennestWebsite/CennestWebsite/Scripts/framework-view.js
@@ -0,0 +1,97 @@
+﻿var $main = $('#main'), $header = $('.header');
+var $frame = $main.find('.frame');
+var $slidee = $frame.find('.slidee');
+var $sly;
+
+var slyHorizontalOptions = {
+    horizontal: 1,
+    itemNav: 'centered',
+    smart: 1,
+    activateOn: 'click',
+    mouseDragging: 1,
+    touchDragging: 1,
+    releaseSwing: 1,
+    scrollBy: 1,
+    speed: 300,
+    elasticBounds: 1,
+    dragHandle: 1,
+    dynamicHandle: 1,
+    clickBar: 1,
+};
+
+var FrameworkView = {
+    init: function () {
+        $frameworkList = FrameworkList.getAllFrameworks();
+        $frameworkList.forEach(function (framework) {
+            $slidee.append('<li id="' + framework.id + '">' +
+                        '<div class="tile-container">'+
+                        '<div class="tile-button">'+
+                            '<div class="text-container">' +
+                                    '<div class="top-text">' + framework.Tiles.text + '</div>' +
+                                '</div>' +
+                                '<div class="icon-container ' + framework.Tiles.imageCssClass + '">' +
+                                '</div>' +
+                        '</div>'+
+                    '</div>'+
+                    '</li>');
+        });
+        if ($sly) {
+            this.destroySly();
+        }
+        $sly = new Sly($frame, slyHorizontalOptions).init();
+        $header.removeClass('gridheader');
+        $main.removeClass('grid');
+        $main.animate().addClass('horizontalgrid');
+        $frame.fadeIn();
+        $sly.reload();
+        
+        this.bindEvent(window, 'resize', this.handleResizeEvent);
+        $slidee.on('click', 'li', this.handleListClick);
+    },
+        destroySly: function () {
+            if ($sly) {
+                $sly.destroy();
+                $sly = null;
+            }
+        },
+bindEvent: function (element, evt, fn) {
+    if (element && evt) {
+        if (typeof evt === 'function') {
+            fn = evt;
+            evt = 'click';
+        }
+        jQuery(element).bind(evt, fn);
+    } else {
+        //log
+    }
+},
+unbindEvent: function (element, evt, fn) {
+    if (element && evt) {
+        if (typeof evt === 'function') {
+            fn = evt;
+            evt = 'click';
+        }
+        jQuery(element).unbind(evt, fn);
+    } else {
+        //log
+    }
+},
+handleResizeEvent: function (e) {
+    if ($sly) {
+        $sly.reload();
+    }
+},
+handleListClick: function () {
+    console.log('project framework of ' + this.id);
+}
+
+}
+
+var HighlightTab = {
+    init: function () {
+        var $frameworkLi = $("#liFramework");
+        $frameworkLi.addClass("active");
+    }
+}
+
+
---
+++ CennestWebsite/CennestWebsite/Scripts/sly/GenerateRandomColors.js
@@ -0,0 +1,35 @@
+﻿
+//Get Random Color
+random = function (i, total) {
+    var hex = '0123456789ABCDEF'.split(''),
+         color = '#', i;
+    for (i = 0; i < total ; i++) {
+        color = color + hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};  
+
+//Generate dark/light color
+//hex — a hex color value such as “#abc” or “#123456″ (the hash is optional)
+//lum — the luminosity factor, i.e. -0.1 is 10% darker, 0.2 is 20% lighter, etc.
+function colorLuminance(hex, lum) {
+    // validate hex string
+    hex = String(hex).replace(/[^0-9a-f]/gi, '');
+    if (hex.length < 6) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    lum = lum || 0;
+
+    // convert to decimal and change luminosity
+    var rgb = "#", c, i;
+    for (i = 0; i < 3; i++) {
+        c = parseInt(hex.substr(i * 2, 2), 16);
+        c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+        rgb += ("00" + c).substr(c.length);
+    }
+
+    return rgb;
+}
+
+
+
